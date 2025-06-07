28efa9face4cd3e15f76aa3a20312e31b826bf44
Yannis <yannis.jean@gmail.com>
2025-06-07 22:30:33 +0200

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
+// import ws from "ws";
 import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
